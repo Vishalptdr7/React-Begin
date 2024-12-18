1ba981d05330cb0157fb56a8ea2bd9add985dd33
Vishal Patidar <patidarvishal233@gmail.com>
2024-12-18 16:12:45 +0530

    last commit of the day

--- README.md
+++ README.md
@@ -6,3 +6,32 @@
 - Image Optimization
 - Bundling
 - Compress
+
+
+# JSX
+- it is a js symbol
+- It is not a javascript
+- It is same as javasript
+- By the help of JSX we can easily create react element 
+
+
+# Babel
+- it convert the JSX code in which the js engine is understood or browser understand
+- it  is a javascript complier
+
+
+
+# components
+- Two types
+  1. Class based components  - OLD way to create a component
+
+  2. Functional components    - New way to create a component  
+ => it is a javascript function to execute a a block of jsx code 
+
+
+- Every thing inside the react is component 
+example
+- A card is a component, A header is a component ,A footer is a component, A group of card is also a component 
+
+# component composition
+- component inside a component is called component composition
--- app.js
+++ app.js
@@ -8,18 +8,18 @@ import ReactDOM from "react-dom/client";
 
 
 
-// const parent = React.createElement(
-//   "div",
-//   { id: "parent" },
-//   React.createElement(
-//     "div",
-//     { id: "child" },
-//     React.createElement("h1", {}, "Jai Shree Ram")
-//   )
-// );
-// console.log(parent);
-// const root1 = ReactDOM.createRoot(document.getElementById("js"));
-// root1.render(parent);
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "Jai Shree Ram")
+  )
+);
+console.log(parent);
+const root1 = ReactDOM.createRoot(document.getElementById("js"));
+root1.render(parent);
 
 const parent1 = React.createElement(
   "div",
@@ -35,8 +35,8 @@ root2.render(parent1);
 console.log(parent1);
 
 const jsxheading = (<h1 id="head">
-   Hello World from React with JSX 
-   </h1>);
+  Hello World from React with JSX 
+  </h1>);
 
 const root=ReactDOM.createRoot(document.getElementById("js"))
 root.render(jsxheading)
@@ -50,3 +50,54 @@ const rootname=ReactDOM.createRoot(document.getElementById("center"));
 rootname.render(name);
 
 
+// react component->
+
+const HelloWorld = () => {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+    </div>
+  );
+};
+
+const roothello = ReactDOM.createRoot(document.getElementById("hello"));
+
+roothello.render(<HelloWorld />);
+
+
+const Firstcompo=()=>(
+  <div>
+    <h1>This is a first component</h1>
+  </div>
+
+);
+const Secondcompo = () => (
+  <div>
+    <Firstcompo />
+    <h1>This is a second component</h1>
+  </div>
+);
+
+const rootsecond = ReactDOM.createRoot(document.getElementById("second"));
+
+rootsecond.render(<Secondcompo />);
+
+
+
+
+//// Create a element and put inside the component
+const ele=(
+  <div>
+    <h1>This is a Element </h1>
+    <p>This is a paragraph</p>
+  </div>
+);
+const Thirdcompo=()=>(
+  <div>
+    {ele}
+    <h1>This is a third component</h1>
+  </div>
+);
+const rootThird=ReactDOM.createRoot(document.getElementById("eleInsideCompo"));
+
+rootThird.render(<Thirdcompo />);
