167546954d5c60d1cf805241f35cc6637ef5897a
Vishal Patidar <patidarvishal233@gmail.com>
2024-12-18 15:09:03 +0530

    second commit of jsx

--- app.js
+++ app.js
@@ -4,33 +4,49 @@ import ReactDOM from "react-dom/client";
 
 
 
-const to = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from react"
-);
 
-console.log(to);
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", {}, "Jai Shree Ram")
-  )
-);
-const root1 = ReactDOM.createRoot(document.getElementById("js"));
-root1.render(parent);
+
+
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement(
+//     "div",
+//     { id: "child" },
+//     React.createElement("h1", {}, "Jai Shree Ram")
+//   )
+// );
+// console.log(parent);
+// const root1 = ReactDOM.createRoot(document.getElementById("js"));
+// root1.render(parent);
 
 const parent1 = React.createElement(
   "div",
   { id: "parent1" },
   React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "Jai Shree Mahakal"),
-    React.createElement("h2", {}, "Jai Shree Shyam"),
+    React.createElement("h1", { key: "mahakal" }, "Jai Shree Mahakal"),
+    React.createElement("h2", { key: "shyam" }, "Jai Shree Shyam"),
   ])
 );
+
 const root2 = ReactDOM.createRoot(document.getElementById("js1"));
 root2.render(parent1);
+console.log(parent1);
+
+const jsxheading = (<h1 id="head">
+   Hello World from React with JSX 
+   </h1>);
+
+const root=ReactDOM.createRoot(document.getElementById("js"))
+root.render(jsxheading)
+console.log(jsxheading)
+
+
+const name=(<h1 className="name">
+  Vishal Patidar
+  </h1>);
+const rootname=ReactDOM.createRoot(document.getElementById("center"));
+rootname.render(name);
+
+
