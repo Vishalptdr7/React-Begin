bdaa3ed54fa6705fb643788dcf6a0eca8e3de4df
Vishal Patidar <patidarvishal233@gmail.com>
2025-01-08 18:38:58 +0530

    commited on

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,71 +6,57 @@ import Shimmer from "./Shimmer.js";
 
 
 const Body = () => {
-    const [cardDataa,setCardData] = useState(cardData);
-    
-    useEffect(()=>{
-      fetchData();
-    },[]);
-    const fetchData=async()=>{
-    const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.99740&lng=79.00110&collection=80463&tags=layout_BAU_Contextual%2Cnoodles&sortBy=&filters=&type=rcv2&offset=0&page_type=null"
+    const [cardDataa, setCardData] = useState(cardData);
+    const [filterData, setFilterData] = useState(cardData);
+    const [searchText, setSearchText] = useState("");
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        const data = await fetch(
+            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.99740&lng=79.00110&collection=80463&tags=layout_BAU_Contextual%2Cnoodles&sortBy=&filters=&type=rcv2&offset=0&page_type=null"
+        );
+        const json = await data.json();
+        console.log(json);
+    };
+
+    return cardDataa.length === 0 ? (
+        <Shimmer />
+    ) : (
+        <div className="body">
+            <div className="filter">
+                <div className="search-container">
+                    <input
+                        type="text"
+                        placeholder="Search here..."
+                        className="search-input"
+                        value={searchText}
+                        onChange={(e) => setSearchText(e.target.value)}
+                    />
+                    <button
+                        className="search-button"
+                        onClick={() => {
+                            const filteredData = cardDataa.filter((data) =>
+                                data.name.toLowerCase().includes(searchText.toLowerCase())
+                            );
+                            setFilterData(filteredData);
+                        }}
+                    >
+                        Search
+                    </button>
+                </div>
+            </div>
+
+            <div className="resto-container">
+                {filterData.map((data) => (
+                    <RestoCard key={data.id} restData={data} />
+                ))}
+            </div>
+        </div>
     );
-    const json = await data.json();
-    console.log(json);
-    
-    }
-    
-    
-    
-    
-    
-  return (cardDataa.length)===0 ? <Shimmer></Shimmer>:(
-    <div className="body">
-      <div className="search-container">
-        <input
-          type="text"
-          placeholder="Search here..."
-          className="search-input"
-        />
-        <button className="search-button">Search</button>
-      </div>
-      {/* Shimmer */}
-      
-
-
-      {/* Filter data on button */}
-      <div className="filter">
-        {}
-        <button
-          className="filter-btn"
-          onClick={() => {
-            const fileterdData = cardDataa.filter((data) => data.rating > 4.4);
-
-            setCardData(fileterdData);
-          }}
-        >
-          Get Top
-        </button>
-      </div>
-      <div className="resto-container">
-        {/* we create a map function to get all the data inside the carddata object */}
-
-        {cardDataa.map((data) => (
-          <RestoCard key={data.id} restData={data} />
-        ))}
-
-        {/* {cardData.map((card) => (
-          <RestoCard
-            key={card.id}
-            image={card.image}
-            name={card.name}
-            description={card.description}
-            rating={card.rating}
-            time={card.time}
-          />
-        ))} */}
-      </div>
-    </div>
-  );
 };
 export default Body;
+
+
